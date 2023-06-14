d8e999d6d5cdf7d6420f9d69c0e58c9efe1f938e
JakobDylanC <jakobdylanc@gmail.com>
2023-06-14 07:34:19 -0400

    first commit

---
+++ gpt4_stuff.py
@@ -0,0 +1,71 @@
+import openai
+import tiktoken
+import backoff
+import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+if "OPENAI_API_KEY" not in os.environ:
+    raise ValueError(f"Required environment variable OPENAI_API_KEY is not set.")
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+GPT4_MODEL = "gpt-4"
+GPT4_TOKENS_PER_MESSAGE = 3
+GPT4_TOKENS_PER_NAME = 1
+GPT4_TOKENS_PER_MESSAGES = 3
+GPT4_MAX_TOTAL_TOKENS = 8192
+MAX_COMPLETION_TOKENS = 1024 #customize here, must be less than GPT4_MAX_TOTAL_TOKENS
+MAX_PROMPT_TOKENS = GPT4_MAX_TOTAL_TOKENS - MAX_COMPLETION_TOKENS - GPT4_TOKENS_PER_MESSAGES
+TIMEOUT_SECONDS = 120
+encoding = tiktoken.get_encoding("cl100k_base")
+
+@backoff.on_exception(backoff.expo, openai.error.RateLimitError)
+async def chat_completion(system_prompt, msgs):
+    print(f"Generating GPT response for prompt:\n{msgs[-1]['content']}")
+    response_content = "Sorry, an error occurred. Please try again."
+    try:
+        response = await asyncio.wait_for(
+            openai.ChatCompletion.acreate(
+                model=GPT4_MODEL,
+                messages=[system_prompt]+msgs,
+                max_tokens=MAX_COMPLETION_TOKENS,
+        ), timeout = TIMEOUT_SECONDS)
+        response_content = response["choices"][0]["message"]["content"].strip()
+        print(f"GPT response:\n{response_content}")
+        print(f"Prompt tokens: {response['usage']['prompt_tokens']}  Completion tokens: {response['usage']['completion_tokens']}  Total tokens: {response['usage']['total_tokens']}")
+    except asyncio.TimeoutError:
+        print("Timeout exceeded.")
+    return response_content
+
+@backoff.on_exception(backoff.expo, openai.error.RateLimitError)
+async def chat_completion_stream(system_prompt, msgs):
+    print(f"Generating GPT response for prompt:\n{msgs[-1]['content']}")
+    async for chunk in await openai.ChatCompletion.acreate(
+        model=GPT4_MODEL,
+        messages=[system_prompt]+msgs,
+        max_tokens=MAX_COMPLETION_TOKENS,
+        stream=True,
+    ):
+        delta = chunk["choices"][0].get("delta", {})
+        yield delta
+
+def count_tokens(msg):
+    num_tokens = GPT4_TOKENS_PER_MESSAGE
+    for key, value in msg.items():
+        num_tokens += len(encoding.encode(value))
+        if key == "name":
+            num_tokens += GPT4_TOKENS_PER_NAME
+    return num_tokens
+
+def split_string_clean(string, max_length):
+    if len(string) <= max_length:
+        return [string]
+    string_chunks = []
+    start_index = 0
+    while start_index < len(string):
+        end_index = min(start_index + max_length, len(string))
+        split_index = string.rfind(" ", start_index, end_index) if end_index < len(string) else end_index
+        end_index = split_index if split_index != -1 else end_index
+        string_chunks.append(string[start_index:end_index])
+        start_index = end_index + 1 if split_index != -1 else end_index
+    return string_chunks
---
+++ main.py
@@ -0,0 +1,93 @@
+import gpt4_stuff
+import discord
+import asyncio
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+load_dotenv()
+REQUIRED_ENV_VARS = ["DISCORD_BOT_TOKEN", "CUSTOM_SYSTEM_PROMPT"]
+if (missing_env_vars := [var for var in REQUIRED_ENV_VARS if var not in os.environ]):
+    raise ValueError(f"Required environment variables are not set: {', '.join(missing_env_vars)}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+DISCORD_MSG_LENGTH_LIMIT = 2000
+
+class MsgNode:
+    def __init__(self, msg, reply_to=None):
+        self.msg = msg
+        self.tokens = gpt4_stuff.count_tokens(msg)
+        self.reply_to = reply_to
+
+    def get_reply_chain(self, system_prompt_tokens):
+        chain = []
+        num_tokens = system_prompt_tokens
+        current_msg = self
+        while current_msg != None:
+            num_tokens += current_msg.tokens
+            if num_tokens > gpt4_stuff.MAX_PROMPT_TOKENS:
+                break
+            chain.append(current_msg.msg)
+            current_msg = current_msg.reply_to
+        print(f"Calculated prompt tokens: {num_tokens + gpt4_stuff.GPT4_TOKENS_PER_MESSAGES}")
+        return chain[::-1]
+msg_nodes = {}
+
+@bot.event
+async def on_message(message):
+    if bot.user not in message.mentions or message.author.bot:
+        return
+
+    user_prompt_content = message.content.replace(bot.user.mention, "", 1).strip()
+    if not user_prompt_content:
+        return
+    
+    async with message.channel.typing():
+        msg_nodes[message.id] = MsgNode({"role": "user", "content": user_prompt_content, "name": str(message.author.id)})
+        
+        if message.reference and message.reference.message_id in msg_nodes:
+            msg_nodes[message.id].reply_to = msg_nodes[message.reference.message_id]
+        elif message.reference:
+            try:
+                ref_msg = await message.channel.fetch_message(message.reference.message_id)
+                if ref_msg.content:
+                    author_role = "assistant" if ref_msg.author == bot.user else "user"
+                    msg_nodes[ref_msg.id] = MsgNode({"role": author_role, "content": ref_msg.content, "name": str(ref_msg.author.id)})
+                    msg_nodes[message.id].reply_to = msg_nodes[ref_msg.id]
+            except discord.DiscordException:
+                print("Error fetching the referenced message")
+
+        current_date = datetime.now().strftime("%B %d, %Y")
+        system_prompt_content = f"{os.environ['CUSTOM_SYSTEM_PROMPT']}\nKnowledge cutoff: Sep 2021. Current date: {current_date}"
+        system_prompt = {"role": "system", "content": system_prompt_content}
+        system_prompt_tokens = gpt4_stuff.count_tokens(system_prompt)
+        msgs = msg_nodes[message.id].get_reply_chain(system_prompt_tokens)
+        if msgs:
+            response_messages = []
+            response = ""
+            edit_message_task = None
+            previous_delta = None
+            async for current_delta in gpt4_stuff.chat_completion_stream(system_prompt, msgs):
+                if previous_delta != None:
+                    current_delta_content = current_delta.get("content", "")
+                    previous_delta_content = previous_delta.get("content", "")
+                    if previous_delta_content != "":
+                        if response_messages == [] or len(response_message_content+previous_delta_content) > DISCORD_MSG_LENGTH_LIMIT:
+                            response_messages.append(await message.reply(content=previous_delta_content))
+                            response_message_content = ""
+                        response_message_content += previous_delta_content
+                        response += previous_delta_content
+                        if edit_message_task is None or edit_message_task.done() or len(response_message_content+current_delta_content) > DISCORD_MSG_LENGTH_LIMIT or current_delta == {}:
+                            while isinstance(edit_message_task, asyncio.Task) and not edit_message_task.done():
+                                await asyncio.sleep(0)
+                            edit_message_task = asyncio.create_task(response_messages[-1].edit(content=response_message_content))
+                previous_delta = current_delta
+            for response_message in response_messages:
+                msg_nodes[response_message.id] = MsgNode({"role": "assistant", "content": response}, reply_to=msg_nodes[message.id])
+
+async def main():
+    await bot.start(os.environ["DISCORD_BOT_TOKEN"])
+
+if __name__ == "__main__":
+    asyncio.run(main())
